27c61c4eb84ddd780dcb0e2e834d3dddbb05e827
Bhuvnesh Khandelwal <bhuvnesh.kh1997@gmail.com>
2016-10-27 17:34:08 +0530

    Update events.php

--- events.php
+++ events.php
@@ -1,7 +1,65 @@
 <?php
 include "php/top.php";
 ?>
+<div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast">Events</div>
+          <div class="android-card-container mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/campus_ambassador.png">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Start-Up-er The Startup Job Fair</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead">Startup campus placement programme will see the participation of 25+ startups backed by prestigious Venture Capital Firms like Saif Partners, Matrix Partners, Blume Ventures etc. offering jobs in various profiles like tech, design, content development, business development and marketing.</span>
+              </div>
+              <div class="mdl-card__actions">
+                 <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="http://www.edciitd.com/job1.html">
+                   Check more Details
+                   <i class="material-icons">chevron_right</i>
+                 </a>
+              </div>
+            </div>
 
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/startup_showcase.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Startup Showcase</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead">Entrepreneurship Development Cell (eDC), IIT Delhi is proud to present its flagship event Startup Showcase Competition , in association with Foradian Technologies and LetsVenture. A 3-tier competition which is to conclude on 8th March at IIT Delhi, Startup Showcase aims to provide a robust and efficacious platform for the future of the Indian Startup Ecosystem to showcase their budding ventures to the top mentors and investors from India and abroad.</span>
+              </div>
+              <div class="mdl-card__actions">
+                 <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="http://www.edciitd.com/startupShowcase1.html">
+                   Show'em all
+                   <i class="material-icons">chevron_right</i>
+                 </a>
+              </div>
+            </div>
+
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/stock_market.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Virtual Stock Market Game</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead">eDC IIT Delhi comes up with its ‘Stock Star’ to create new “Wolf of Wall Street”. This program or application that attempts to reproduce or duplicate some or all features of a live stock market on a computer so that a player may practice trading stocks without financial risk.</span>
+              </div>
+              <div class="mdl-card__actions">
+                 <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="http://www.edciitd.com/stockMarket1.html">
+                   More Information
+                   <i class="material-icons">chevron_right</i>
+                 </a>
+              </div>
+            </div>
+			
+          </div>
+        </div>
 <?php
 	include "php/bottom.php";
 ?>
