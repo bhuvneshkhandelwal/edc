bdb0de6284595d0e82218aa932aab9b6b7d430f1
Bhuvnesh Khandelwal <bhuvnesh.kh1997@gmail.com>
2016-10-27 17:14:57 +0530

    Update team.php
    team.php by Bhuvnesh

--- team.php
+++ team.php
@@ -1,7 +1,345 @@
 <?php
 include "php/top.php";
 ?>
-
+<div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast" align="center">Meet The Team EDC IITD</div>
+ </div>
+ <div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast"><h4>Faculty Advisor</h4></div>
+          <div class="android-card-container mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Prashant_Mishra.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Prashant Mishra</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead">Email id:pmishra@dbeb.iitd.ac.in</span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+          </div>
+        </div>
+         <div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast"><h4>Industry Advisor</h4></div>
+          <div class="android-card-container mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Sanjiv_Sethi.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Dr.Sanjiv Sethi</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead">Identified as one of the Top 12 Managers and Prospective Business Leader - (All India) by CII, FICCI, AIMA, ASSOCHAM, Foreign and commonwealth Office and British Council : British Chevening Scholarship- Managers for Leadership Program-UK , This is One of the most Prestigious Scholarships in the Globe . He is known for this practical and and real world transformative perspective and is a constant source of guidance and mentorship to eDC IIT Delhi.</span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+          </div>
+        </div>
+         <div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast"><h4>Overall Coordinators</h4></div>
+          <div class="android-card-container mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Apurv.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Apurv Sibal</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+              <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Abhijeet.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Abhijeet Malhotra</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+          </div>
+        </div>
+                 <div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast"><h4>Marketing</h4></div>
+          <div class="android-card-container mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Arushi.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Arushi Goel</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+              <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Shubham.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Shubham Goel</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast"><h4>Corporate and Industrial Relations</h4></div>
+          <div class="android-card-container mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Advait.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Advait Bhagwat</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+              <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Kunal.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Kunal Aggarwal</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+             <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Manuj.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Manuj Khullar</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast"><h4>Student Entrepreneurship Network</h4></div>
+          <div class="android-card-container mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Akshay.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Akshay Royal</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+              <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Krati.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Krati Rathore</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+             <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Sumit.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Sumit Mishra</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast"><h4>Events and Operations</h4></div>
+          <div class="android-card-container mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Ashwin.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Ashwin Bhola</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+              <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Navneet.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Navneet Bihani</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+             <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Vageesh.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Vageesh Gupta</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+          </div>
+        </div>
+                   <div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast"><h4>Publicity</h4></div>
+          <div class="android-card-container mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Abhinav.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Abhinav Yadav</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+              <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Sonali.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Sonali Godara</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+          </div>
+        </div>
+         <div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast"><h4>Editorial</h4></div>
+          <div class="android-card-container mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Sanjana.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Sanjana Gupta</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+              <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Siddhartha.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Siddhartha Pradeep</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="android-more-section" id="events">
+          <div class="android-section-title mdl-typography--display-1-color-contrast"><h4>Events and Operations</h4></div>
+          <div class="android-card-container mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Anshuman.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Anshuman Srivastava</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+              <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Rahul.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Rahul Gupta</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+             <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+              <div class="mdl-card__media">
+                <img src="images/team/Shivank.jpg">
+              </div>
+              <div class="mdl-card__title">
+                 <h4 class="mdl-card__title-text">Shivank Goel</h4>
+              </div>
+              <div class="mdl-card__supporting-text">
+                <span class="mdl-typography--font-light mdl-typography--subhead"></span>
+              </div>
+              <div class="mdl-card__actions">
+              </div>
+            </div>
+          </div>
+        </div>
 <?php
 	include "php/bottom.php";
 ?>
